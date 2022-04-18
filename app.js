8612a98a759d5edc751c16dbdc5a0e2faae8bd67
Nick Fulghum <nickfulghum92@gmail.com>
2022-04-18 17:02:22 -0500

    add basic styles

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`
+    return `rgb(${r},${g},${b})`;
 }
 
 const letters = document.querySelectorAll('.letter');
 const intervalId = setInterval(function () {
     for (let letter of letters) {
-        letter.getElementsByClassName.color = randomRGB();
+        letter.style.color = randomRGB();
     }
 }, 1000);
